97c7be17c796a23e5823d302f572e4c8018bd779
yunusmujadidi <yunusmujadidi@example.com>
2025-07-17 18:29:26 +0700

    chore: setup project structure

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,4 @@
+export const RegisterPage = () => {
+  return <div>Register Page</div>;
+};
+export default RegisterPage;
---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+      <div className="text-2xl text-blue-300">Hello world!</div>
+      <Button variant="outline">Click Here</Button>
+    </>
+  );
+};
+
+export default Home;
