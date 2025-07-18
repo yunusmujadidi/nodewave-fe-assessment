9f4348505e96ffe0539db513f3757ae8ed4856f7
yunusmujadidi <yunusmujadidi@example.com>
2025-07-18 20:26:24 +0700

    feat: add auth store using zustand

---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface User {
+  id: string;
+  email: string;
+  name: string;
+}
+
+interface AuthStore {
+  user: User | null;
+  token: string | null;
+  isAuthenticated: boolean;
+
+  login: (user: User, token: string) => void;
+  logout: () => void;
+}
+
+export const useAuth = create<AuthStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      isAuthenticated: false,
+      login: (user, token) => {
+        localStorage.setItem("nodewave-token", token);
+        set({ user, token, isAuthenticated: true });
+      },
+      logout: () => {
+        localStorage.removeItem("nodewave-token");
+        set({ user: null, token: null, isAuthenticated: false });
+      },
+    }),
+    {
+      name: "nodewave-auth",
+    }
+  )
+);
